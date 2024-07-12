1154db53fd379c7b3ae84dc385934bb9c7b5b50f
iOSD3v <nikkhah.fahimeh@gmail.com>
2024-07-12 11:10:44 +0330

    Initial Commit

---
+++ TodoTestApp/TodoTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTestAppApp.swift
+//  TodoTestApp
+//
+//  Created by Fahimah on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
