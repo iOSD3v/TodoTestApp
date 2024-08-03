06990fda97406bb6f1de1fa75e48dc3776461c3b
iOSD3v <nikkhah.fahimeh@gmail.com>
2024-08-03 12:10:29 +0330

    Add alert for todo

--- TodoTestApp/ContentView.swift
+++ TodoTestApp/ContentView.swift
@@ -8,16 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var todos: [String] = [
+        "go to gym",
+        "warm up",
+        "tredmill",
+        "bike",
+        "bolgarian lounge",
+        "squat",
+        "deadlift",
+    ]
+    func addItem() {
+        todos.append(newItem)
+        newItem = ""
+    }
+    
+    @State var showingAlert = false
+    @State var newItem = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        NavigationStack {
+            List($todos, id: \.self, editActions: .all) { $item in
+                Text(item)
+            }
+            .navigationTitle("My To-do List")
+            .navigationBarItems(
+                trailing: Button(action: {
+                    showingAlert = true
+                }, label: {
+                    Text("Add")
+                })
+            )
+            
+        }.alert("what are you gonna do?", isPresented: $showingAlert) {
+            TextField("write here", text: $newItem)
+            Button(action: {
+                addItem()
+            }, label: {
+                Text("OK")
+            })
+                
+            }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
